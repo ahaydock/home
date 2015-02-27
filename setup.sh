fefa0f3bece7150e899871c56cfa3544a8197f4a
Andrew Haydock <ahaydock@gmail.com>
2015-02-26 19:54:50 -0800

    Updating setup.sh with different packages and disabling puppet.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # packages
-PKGS='curl git git-flow nmap puppet rubygems tree vagrant vim vim-athena wget'
-GEMS='rake hiera-eyaml puppet-lint puppetlabs_spec_helper'
+PKGS='curl git git-flow nmap nmon rubygems tree vagrant vim vim-athena wget sysstat blktrace iotop'
+GEMS='rake'
 XPKGS='synergy geany geany-plugins pidgin pidgin-sipe conky'
 
 # Install dotfiles
@@ -11,11 +11,11 @@ for F in .bashrc .profile .inputrc .vimrc .dircolors ; do
 done
 
 # Setup puppet repo
-PUPPET_URL="http://apt.puppetlabs.com/";
-DISTRIB_CODENAME=`awk -F= '/^DISTRIB_CODENAME/ { print $2 }' /etc/lsb-release`;
-PUPPET_REPO="puppetlabs-release-$DISTRIB_CODENAME.deb";
-/usr/bin/wget -O /tmp/$PUPPET_REPO $PUPPET_URL/$PUPPET_REPO 2>&1 >/dev/null;
-/usr/bin/dpkg -i /tmp/$PUPPET_REPO;
+# PUPPET_URL="http://apt.puppetlabs.com/";
+# DISTRIB_CODENAME=`awk -F= '/^DISTRIB_CODENAME/ { print $2 }' /etc/lsb-release`;
+# PUPPET_REPO="puppetlabs-release-$DISTRIB_CODENAME.deb";
+# /usr/bin/wget -O /tmp/$PUPPET_REPO $PUPPET_URL/$PUPPET_REPO 2>&1 >/dev/null;
+# /usr/bin/dpkg -i /tmp/$PUPPET_REPO;
 
 # install silently
 echo force-confold >> /etc/dpkg/dpkg.cfg.d/force_confold;
